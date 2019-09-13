debd3bd6f5c46b5b651c5b5d32ee06f3f14e92c2
OrangeySnicket <55061391+OrangeySnicket@users.noreply.github.com>
2019-09-13 12:30:00 -0600

    Block textures work while placed, but not in inventory.

--- workspace/InvestitureMod/src/main/java/block/Tiles.java
+++ workspace/InvestitureMod/src/main/java/block/Tiles.java
@@ -1,6 +1,7 @@
 package block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
--- workspace/InvestitureMod/src/main/java/block/TinStairs.java
+++ workspace/InvestitureMod/src/main/java/block/TinStairs.java
@@ -1,6 +1,7 @@
 package block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockStairs;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
